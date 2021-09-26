15b459c244e3ac1c5844f7e1c1979494f2aa01d8
febryyadi.z <febryyadi.z@yahoo.com>
2021-09-27 00:05:35 +0800

    Add project files.

---
+++ MVCDEmo/Models/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDEmo.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; } = 0;
+        public bool IsAlive { get; set; } = true;
+    }
+}
